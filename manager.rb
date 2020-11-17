ef5f4e00e3849f0a2ef74fb6d33bad427bcf7d36
ariel taylor <ariel.taylorr@gmail.com>
2020-11-17 11:40:12 -0800

    Calculate bonuses

--- manager.rb
+++ manager.rb
@@ -1,4 +1,5 @@
 require_relative "./employee.rb"
+require "byebug"
 
 class Manager < Employee
     attr_reader :name, :title, :salary, :boss
@@ -10,12 +11,19 @@ class Manager < Employee
     end
 
     def bonus_multiplier(multiplier)
+        # debugger
         queue = @employees
+        # debugger
         sum = 0
+        # debugger
         until queue.empty?
+            # debugger
             person = queue.shift
+            # debugger
             sum += person.salary * multiplier
-            queue << person.employees
+            # debugger
+            queue += person.employees if person.class == Manager
+            # debugger
         end
         sum
     end
@@ -32,6 +40,6 @@ ned = Manager.new("Ned", "Founder", 1000000)
 darren = Manager.new("Darren", "TA Manager", 78000, ned)
 shawna = Employee.new("Shawna", "TA", 12000, darren)
 david = Employee.new("David", "TA", 10000, darren)
-# ned.bonus_multiplier(5)
-darren.bonus_multiplier(4)
-david.bonus_multiplier(3)
+p ned.bonus_multiplier(5)
+p darren.bonus_multiplier(4)
+p david.bonus_multiplier(3)
